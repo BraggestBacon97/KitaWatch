2a4c98d1c7e187fa7d6525e25222c2906ea28fa1
Koya Nishio <foxynoxy07@gmail.com>
2026-09-18 13:00:16 +0200

    Update version to ver. 0.5.1

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -132,7 +132,7 @@ export default function Sidebar() {
         </div>
         <Disclaimer />
         <p className="text-[10px] uppercase tracking-widest text-zinc-700">
-          v0.5.0
+          v0.5.1
         </p>
       </div>
     </motion.aside>
