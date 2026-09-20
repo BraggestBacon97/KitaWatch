c0dbe53477d2527ead466f3096e7a4cc1ff43996
Koya Nishio <foxynoxy07@gmail.com>
2026-09-20 11:21:09 +0200

    v0.6.6

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -132,7 +132,7 @@ export default function Sidebar() {
         </div>
         <Disclaimer />
         <p className="text-[10px] uppercase tracking-widest text-zinc-700">
-          v0.6.5
+          v0.6.6
         </p>
       </div>
     </motion.aside>
